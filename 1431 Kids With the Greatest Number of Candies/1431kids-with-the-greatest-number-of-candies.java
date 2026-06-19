9b55b59db7f12f18040475add0c63cf56ed05bcd
ishitkanojia <kanojiaishit@gmail.com>
2026-06-19 07:14:23 +0000

    Code challenge submission for question: 1431 Kids With the Greatest Number of Candies

---
+++ 1431 Kids With the Greatest Number of Candies/1431kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result= new ArrayList<>();
+        int greatestCandies = 0;
+        for(int i=0;i<candies.length;i++) {
+            if (candies[i]>greatestCandies) {
+                greatestCandies=candies[i];
+            }
+        }
+        for(int i=0;i<candies.length;i++) {
+            if (candies[i]+extraCandies >= greatestCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
