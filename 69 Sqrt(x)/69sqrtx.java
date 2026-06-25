6ed919eab1adadcc73f2a66841e7872dd7deeb76
ishitkanojia <kanojiaishit@gmail.com>
2026-06-25 18:10:17 +0000

    Code challenge submission for question: 69 Sqrt(x)

---
+++ 69 Sqrt(x)/69sqrtx.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x<2) return x;
+        int l=1, r=x/2;
+        while (l<=r) {
+            int mid=l+(r-l)/2;
+            if (mid<=x/mid) {
+                if (mid+1>x/(mid+1)) return mid;
+                l=mid+1;
+            } else {r=mid-1;}
+        }
+        return r;
+    }
+}
