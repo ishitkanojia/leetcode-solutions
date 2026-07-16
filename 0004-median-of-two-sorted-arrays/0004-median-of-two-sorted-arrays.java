e3d31e599f345488a835b13b6b57947c9801c835
Ishit Kanoja <88983007+ishitkanojia@users.noreply.github.com>
2026-07-16 21:36:54 +0530

    Time: 2 ms (47.95%), Space: 48.9 MB (33.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,24 +1,17 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
         int m = nums1.length;
         int n = nums2.length;
-        int left = 0, right = m;
-        while (left <= right) {
-            int partitionX=left+(right-left)/2;
-            int partitionY=(m+n+1)/2-partitionX;
-            int maxLeftX =(partitionX==0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
-            int minRightX=(partitionX==m) ? Integer.MAX_VALUE : nums1[partitionX];
-            int maxLeftY =(partitionY==0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
-            int minRightY=(partitionY==n) ? Integer.MAX_VALUE : nums2[partitionY];
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                if ((m + n) % 2 == 0) return ((double) Math.max(maxLeftX, maxLeftY)
-                            + Math.min(minRightX, minRightY)) / 2;
-                else return Math.max(maxLeftX, maxLeftY);
-            }
-            else if (maxLeftX > minRightY) right = partitionX - 1;
-            else left = partitionX + 1;
+        int[] merged = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) merged[k++] = nums1[i++];
+            else merged[k++] = nums2[j++];
         }
-        throw new IllegalArgumentException("Input arrays are not sorted");
+        while (i < m) merged[k++] = nums1[i++];
+        while (j < n) merged[k++] = nums2[j++];
+        int total = m + n;
+        if (total % 2 == 1) return merged[total / 2];
+        return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
     }
 }
